8e8f04c284acbd2b0c9409797b33ed7b92ea877f
BRENO-MORAIS <brenofranciscomorais@gmail.com>
2020-12-11 07:59:32 -0400

    Inicial

---
+++ MVCMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public string Index()
+        //{
+        //    return "Essa é Minha Action Padrão";
+        //}
+
+        //public string Welcome(string name,int  numTimes = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+
+        //public string Welcome(string name , int Id=1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name} ,  ID: {Id}");
+        //}
+
+    }
+}
